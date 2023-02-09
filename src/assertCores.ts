0022465f4d861ef5c0a27d8510b491f6b11d3928
Amir Bilu <amirbilu@gmail.com>
2023-02-09 13:39:23 +0200

    fix cores test

--- src/assertCores.ts
+++ src/assertCores.ts
@@ -1,7 +1,7 @@
 import os from "os";
 
 export default function assertCores(cores: number): void {
-  if (os.cpus().length >= cores) {
+  if (os.cpus().length < cores) {
     throw new Error(
       `Expected at least ${cores} cores but got ${os.cpus().length}`
     );
